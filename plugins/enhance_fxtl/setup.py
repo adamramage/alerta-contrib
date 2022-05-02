dec8a1d83a2a65b268e96918c1536134089f6f11
peter.smith@foxtel.com.au <peter.smith@foxtel.com.au>
2022-05-02 21:11:46 +1000

    add update to runbook url

---
+++ plugins/enhance_fxtl/setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+version = '5.3.3'
+
+setup(
+    name="alerta-enhance-fxtl",
+    version=version,
+    description='Alerta plugin for alert enhancement Foxtel',
+    url='https://github.com/alerta/alerta-contrib',
+    license='MIT',
+    author='Nick Satterly',
+    author_email='peter.smith@foxtel.com.au',
+    packages=find_packages(),
+    py_modules=['alerta_enhance_fxtl'],
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        'alerta.plugins': [
+            'enhance = alerta_enhance:EnhanceAlert'
+        ]
+    }
+)
